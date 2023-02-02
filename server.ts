67b9883ce8ec6a50e40926d20616add29ae72e5a
Samuel Wanjiru <sw@kibramoa.com>
2023-02-02 17:59:42 +0300

    helmet added

--- server.ts
+++ server.ts
@@ -26,8 +26,8 @@ interface OptionsJson {
 dotenv.config();
 const app = express();
 app.use(express.json());
-// app.use(helmet());
-// app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true } as OptionsJson));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
