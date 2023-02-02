c1b5bf297d64d6053f5eea87ff4853d51c4f6a24
Samuel Wanjiru <sw@kibramoa.com>
2023-02-02 17:12:49 +0300

    allow all my domains origin

--- src/config/allowedOrigin.ts
+++ src/config/allowedOrigin.ts
@@ -1,8 +1,1 @@
-export const allowedOrigins = [
-	"http://localhost:3000",
-	"http://localhost:3001",
-	"https://kanban-sm.vercel.app/",
-	"https://kanban-fronend-mentor.vercel.app/",
-	"https://vercel.com/",
-	"*",
-];
+export const allowedOrigins = ["*"];
