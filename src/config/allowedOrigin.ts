71f453803bc69b7c45ab2717fec260ad0f97fc74
Samuel Wanjiru <sw@kibramoa.com>
2023-02-02 14:47:21 +0300

    allowwdOrigins updated for prod v2

--- src/config/allowedOrigin.ts
+++ src/config/allowedOrigin.ts
@@ -1,7 +1,6 @@
 export const allowedOrigins = [
 	"http://localhost:3000",
 	"http://localhost:3001",
-	"https://kanban-fronend-mentor-samuel46.vercel.app",
-	"https://kanbansm.vercel.app/kanban",
-	"https://kanban-fronend-mentor-samuel46.vercel.app",
+	"https://kanban-sm.vercel.app/",
+	"https://kanban-fronend-mentor.vercel.app/",
 ];
